2826133b677dc9519aeb8de83efc6892d58f4439
yaowenkhor <yaowenkhor@gmail.com>
2025-04-30 20:25:43 +0800

    Set up sqlite

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,16 @@
 module.exports = {
     assets: ['./assets/fonts/'],
-  };
+    dependencies: {
+      "react-native-sqlite-storage": {
+        platforms: {
+          android: {
+            sourceDir:
+              "../node_modules/react-native-sqlite-storage/platforms/android-native",
+            packageImportPath: "import io.liteglue.SQLitePluginPackage;",
+            packageInstance: "new SQLitePluginPackage()"
+          }
+        }
+      }
+    }
+  };F
   
