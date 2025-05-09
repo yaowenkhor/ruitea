9b434a331488364f916e503493fb35c254af7c53
Teoh JX <jiaxun040914@gmail.com>
2025-05-09 21:05:36 +0800

    Change screen sequence

--- screens/Orders/OrderTrackingScreen.jsx
+++ screens/Orders/OrderTrackingScreen.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 const OrderTrackingScreen = () => {
   return (
-    <View></View>
+    <View><Text>Order Tracking</Text></View>
   )
 }
 
